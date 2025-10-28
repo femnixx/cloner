a453456696b1e7e1f918addffe290fff797080f8
femnixx <femnitality@gmail.com>
2025-10-28 18:52:15 +0700

    Added authpage and login and signup buttons

---
+++ src/frontend/Authpage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authpage = () => {
+  return (
+    <div className='w-screen h-screen'>
+
+    </div>
+  )
+}
+
+export default Authpage
--- src/frontend/Frontpage.tsx
+++ src/frontend/Frontpage.tsx
@@ -1,7 +1,15 @@
-
 const Frontpage = () => {
   return (
-    <div>Frontpage</div>
+    <div className="w-screen h-screen">
+      <div className="flex gap-x-3 p-5 items-center justify-center">
+        <button className="border p-2 cursor-pointer">
+          sign up
+        </button>
+        <button className="border p-2 cursor-pointer">
+          login
+        </button>
+      </div>
+    </div>
   )
 }
 
