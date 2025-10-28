36693fd6ca54beb019d2d41191475d9bc36fb81c
femnixx <femnitality@gmail.com>
2025-10-28 09:59:25 +0700

    Started on frontend routers

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import auth from './pages/auth'
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/auth' element={<auth />}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
